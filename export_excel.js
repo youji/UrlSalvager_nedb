8ce739b476fee502bd772bb49b75f0aaf0b78550
youji <youji0926@gmail.com>
2022-01-12 17:15:49 +0900

    nedb化

---
+++ export_excel.js
@@ -0,0 +1,66 @@
+const yamlIo = require("./lib/io/yaml");
+const xlsxIo = require("./lib/io/xlsx");
+const PageData = require("./lib/dao/pageData.js");
+const conf = yamlIo.load("conf.yaml");
+const nedb = require('nedb');
+
+
+let db = {};
+db.UrlList = new nedb({ filename: 'result/' + conf.id + '/db', autoload: true });
+db.UrlList.ensureIndex({
+    fieldName: 'url',
+    unique: true
+})
+main();
+
+async function main() {
+    let rows = [];
+    let headerRow =[];
+
+    headerRow = Object.keys(new PageData().info)
+    rows.push(headerRow);
+
+    // console.log(rows)
+
+    let docs = await getDones();
+    docs.forEach(doc => {
+        let tmpRow = [];
+        let pageDataFilePath = "./result/" + conf.id + "/pageData/" + doc['_id'] + '.json';
+        // console.log(pageDataFilePath)
+        try {
+            let pageData = require(pageDataFilePath);
+            let info = pageData.info;
+            headerRow.forEach(item => {
+                tmpRow.push(info[item])
+            });
+        } catch (e) {
+            headerRow.forEach(item => {
+                tmpRow.push("error")
+            });
+        }
+        rows.push(tmpRow)
+    });
+
+    const filename = "./result/" + conf.id + "/" + conf.id + '.xlsx';
+    await xlsxIo.xlsxExport(rows,filename);
+    console.log("ファイル出力完了:"+filename);
+}
+async function getDones() {
+    let rtn = await new Promise((resolve, reject) => {
+        db.UrlList.find({ status: 'done' }).sort({ url: 1 }).exec((err, docs) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(docs);
+            }
+        });
+        // db.UrlList.find({ status: 'done' }, (err, docs) => {
+        //     if (err) {
+        //         reject(err);
+        //     } else {
+        //         resolve(docs);
+        //     }
+        // });
+    });
+    return rtn;
+}
--- salvage.js
+++ salvage.js
@@ -1,70 +1,149 @@
+const fs = require("fs").promises;
 const puppeteer = require("puppeteer");
 const surveyPage = require("./lib/surveyPage");
 const yamlIo = require("./lib/io/yaml");
-const xlsxIo = require("./lib/io/xlsx");
+const jsonIo = require("./lib/io/json");
 const conf = yamlIo.load("conf.yaml");
+const nedb = require('nedb');
 
-// puppeteer
-let browser = null;
+const pageDataDir = "./result/" + conf.id + "/pageData/";
 
-//
-let notYetUrls = new Set(conf.startUrl);
-let processingUrls = new Set();
-let doneData = new Map();
+let db = {};
+db.UrlList = new nedb({ filename: 'result/'+conf.id+'/db', autoload: true });
+db.UrlList.ensureIndex({
+    fieldName: 'url',
+    unique: true
+})
 
+init();
 main();
 
+async function init() {
+    (async () => {
+        await fs.mkdir(pageDataDir, { recursive: true });
+    })();
+
+    if (await countNotYetUrl() > 0) {
+        console.log('前回処理実行時のデータが存在します。途中から開始します。')
+    } else {
+        conf.startUrl.forEach(url => {
+            db.UrlList.insert({
+                url: url,
+                status: 'notyet'
+            }, function (err) {
+                // console.log(err)
+            })
+        });
+    }
+}
 async function main() {
-  console.time("salvage time");
+    console.time("salvage time");
 
-  browser = await puppeteer.launch(conf.browser);
-  //
-  while (notYetUrls.size > 0) {
-    let targetUrl = getNotYetUrl1(notYetUrls);
+    browser = await puppeteer.launch(conf.browser);
+    //
+    let kanryo = await countDoneUrl();
+    let nokori = await countNotYetUrl();
+    while (nokori > 0) {
+        let target = await getNotYetUrl1()
+        let targetId = target._id;
+        let targetUrl = target.url;
 
-    notYetUrls.delete(targetUrl);
-    processingUrls.add(targetUrl);
-    let pageData = await surveyPage.survey(browser, targetUrl);
-    notYetUrls = mergeUrlList(notYetUrls, pageData.hrefs);
-    processingUrls.delete(targetUrl);
+        let pageData = await surveyPage.survey(browser, targetUrl,targetId);
 
-    doneData.set(targetUrl, pageData.info);
-    console.log(notYetUrls.size + " left");
-  }
-  await closeBrowser();
-  await exportXlsxResult();
+        pageData.hrefs.forEach(url => {
+            // console.log(url)
+            if (isAllowDomain(url)) {
+                let insertUrl = url;
+                if(conf.hashDelete){
+                    insertUrl = conf.hashDelete? insertUrl.replace(/#.*$/,"") : insertUrl;
+                }
+                db.UrlList.insert({
+                    url: insertUrl,
+                    status: 'notyet'
+                }, function (err) {
+                    // console.log('skip:' + insertUrl)
+                })
+            }
+        })
 
-  console.timeEnd("salvage time");
+        jsonIo.write(pageDataDir + target._id + '.json', pageData)
+        await updateDone(targetUrl)
+
+        console.log('[残り:' + nokori + ']', '[完了:' + (++kanryo) + ']', '('+targetUrl+' のチェックが終わったところ)')
+
+        nokori = await countNotYetUrl()
+    }
+    await closeBrowser();
+    // await exportXlsxResult();
+
+    console.timeEnd("salvage time");
+}
+function isAllowDomain(url) {
+    let rtn = false;
+    const domain = new URL(url).hostname;
+    if (conf.allowDomain.indexOf(domain) !== -1) {
+        rtn = true;
+    }
+    return rtn;
+}
+async function countNotYetUrl() {
+    let rtn = await new Promise((resolve, reject) => {
+        db.UrlList.count({ status: 'notyet' }, (err, count) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(count);
+            }
+        });
+    });
+    return rtn;
+}
+async function countDoneUrl() {
+    let rtn = await new Promise((resolve, reject) => {
+        db.UrlList.count({ status: 'done' }, (err, count) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(count);
+            }
+        });
+    });
+    return rtn;
+}
+async function getNotYetUrl1() {
+    let rtn = await new Promise((resolve, reject) => {
+        db.UrlList.findOne({ status: 'notyet' }, (err, doc) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(doc);
+            }
+        });
+    });
+    return rtn;
+}
+async function updateDone(url) {
+    let rtn = await new Promise((resolve, reject) => {
+        db.UrlList.update({
+            url: url
+        }, {
+            $set: {
+                status: 'done'
+            }
+        }, (err, numOfDocs) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(numOfDocs);
+            }
+        });
+    });
+    return rtn;
 }
 async function closeBrowser() {
-  await browser.close();
-  console.log("-----CRAWL END-----");
-  return new Promise(function (resolve) {
-    resolve();
-  });
-}
-async function exportXlsxResult() {
-  const formattedData = await xlsxIo.formatExportData(doneData);
-  const filename = await xlsxIo.xlsxExport(formattedData);
-  console.log("-----RESULT EXPORTED (" + filename + ")-----");
-  return new Promise(function (resolve) {
-    resolve();
-  });
-}
-function getNotYetUrl1(set) {
-  for (var value of set) return value;
-}
-function mergeUrlList(baseSet, addSet) {
-  let set = baseSet;
-  for (var url of addSet) {
-    try {
-      const domain = new URL(url).hostname;
-      if (conf.allowDomain.indexOf(domain) !== -1 && !doneData.has(url)) {
-        set.add(url);
-      }
-    } catch (e) {
-      console.log("ERROR:" + url);
-    }
-  }
-  return set;
+    await browser.close();
+    console.log("-----CRAWL END-----");
+    return new Promise(function (resolve) {
+        resolve();
+    });
 }
